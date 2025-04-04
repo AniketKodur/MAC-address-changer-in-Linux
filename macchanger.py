8252469eafb0229ebf64b3cd1f2098ebacc8da1a
AniketKodur <122638437+AniketKodur@users.noreply.github.com>
2025-04-04 22:18:38 +0530

    Update macchanger.py

--- macchanger.py
+++ macchanger.py
@@ -1,68 +1,48 @@
-#!/bin/bash
-import optparse
-from subprocess import *
+#!/usr/bin/env python3
+
 import subprocess as sp
 
-
-
-print("1- eth0\n2- wlan0")
+print("1 - eth0\n2 - wlan0\n3 - enp37s0")
 inputt = int(input("Interface you want to change: "))
 print('\n')
+
 match inputt:
     case 1: interface = "eth0"
-    case 2:interface = "wlan0"
-    case _: print("Please enter the valid input"); exit(1)
-
+    case 2: interface = "wlan0"
+    case 3: interface = "enp37s0"
+    case _: 
+        print("Please enter a valid input") 
+        exit(1)
 
 print("1 - New\n0 - Default")
-ans = int(input("You want new or default:"))
-if ans == 1 or ans == 0:
-    pass
-else:
+ans = int(input("You want new or default: "))
+if ans not in [0, 1]:
     print("Please enter valid input")
     exit(2)
-a = "ifconfig eth0 down"
-c = "ifconfig eth0 up"
-two = "2c:f0:5d:86:6e:28"
 
+a = f"ifconfig {interface} down"
+c = f"ifconfig {interface} up"
+default_mac = "2c:f0:5d:86:6e:28"
 
 if ans == 1:
-    print("\n")
-    print("Note: MAC address should of 12 characters pair of 2 each should be separed by semicolon ( : )")
-    print("Eg. 0a:1b:2c:3d:4e:5f\n")
-    new_mac = input("Enter a mac address: ")
-    length = len(new_mac)
-    couunt = new_mac.count(':')
+    print("\nNote: MAC address should be 12 hex characters (e.g. 0a:1b:2c:3d:4e:5f)\n")
+    new_mac = input("Enter a MAC address: ")
 
-    if length == 17 and couunt == 5:
-        print("done")
-
+    if len(new_mac) == 17 and new_mac.count(':') == 5:
+        print("[+] Changing MAC address for", interface, "to", new_mac)
     else:
-        print("Enter valid MAC address")
+        print("[-] Invalid MAC address format")
         exit(2)
-    print("[+] Changing MAC address for ", interface, "  to ", new_mac)
-    print('\n')
+
     sp.call(a, shell=True)
-    sp.call("ifconfig " +interface+ " hw ether " + new_mac, shell=True)
+    sp.call(f"ifconfig {interface} hw ether {new_mac}", shell=True)
     sp.call(c, shell=True)
-
-
-elif ans == 0:
-    print("[+] Changing MAC address for",interface, "to", two)
-    print('\n')
-    sp.call(a, shell=True)
-    sp.call("ifconfig " +interface+ " hw ether " + two, shell=True)
-    sp.call(c, shell=True)
-
 
 else:
-    print("[+] Changing MAC address for ", interface, "  to ", two)
-    print('\n')
+    print("[+] Reverting MAC address for", interface, "to default:", default_mac)
     sp.call(a, shell=True)
-    sp.call("ifconfig " +interface+ " hw ether " + two, shell=True)
+    sp.call(f"ifconfig {interface} hw ether {default_mac}", shell=True)
     sp.call(c, shell=True)
 
-sp.call("ifconfig",shell=True)
-
-print("Note: If you want the default mac address run this code again and select '0' for Default")
-
+sp.call("ifconfig", shell=True)
+print("Note: To revert to the default MAC address, run the script again and choose option '0'")
